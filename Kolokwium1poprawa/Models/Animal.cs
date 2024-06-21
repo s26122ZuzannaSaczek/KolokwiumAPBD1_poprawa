5e0ef9736d3192993e18c233ec9c6a9b8d7c0d1e
s26122 <s26122@pjwstk.edu.pl>
2024-06-21 14:32:36 +0200

    Initial commit - Models

---
+++ Kolokwium1poprawa/Models/Animal.cs
@@ -0,0 +1,31 @@
+﻿namespace Kolokwium1poprawa.Models;
+
+public class Animal
+{
+    public int ID { get; set; }
+    public string Name { get; set; }
+    public DateTime AdmissionDate { get; set; }
+    public _OwnerID OwnerID { get; set; }
+    public _AnimalClassID AnimalClassID { get; set; }
+    public List<Procedure> Procedures { get; set; }
+}
+
+public class _OwnerID
+{
+    public int ID { get; set; }
+    public string FnirstName { get; set; }
+    public string LastName { get; set; }
+}
+
+public class Procedure
+{
+    public string Name { get; set; }
+    public string Describtion { get; set; }
+    public DateTime Date { get; set; }
+}
+
+public class _AnimalClassID
+{
+    public int ID { get; set; }
+    public string Name { get; set; }
+}
---
+++ Kolokwium1poprawa/Models/NewAnimal.cs
@@ -0,0 +1,9 @@
+﻿namespace Kolokwium1poprawa.Models;
+
+public class NewAnimal
+{
+    public string Name { get; set; }
+    public DateTime AdmissionDate { get; set; }
+    public int OwnerID { get; set; }
+    public int AnimalClassID{ get; set; }
+}
